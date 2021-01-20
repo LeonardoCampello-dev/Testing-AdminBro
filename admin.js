65d803aff8f7bb608be879e02e8f71c503c09264
Leonardo Campello <leonardocampello.dev@gmail.com>
2021-01-20 09:45:53 -0300

    New identation standard

--- admin.js
+++ admin.js
@@ -1,61 +1,61 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
 const ProjectSchema = new mongoose.Schema({
-    title: {
-        type: String,
-        required: true
-    },
-    description: String,
-    completed: Boolean,
-    created_at: { type: Date, default: Date.now }
-})
+  title: {
+    type: String,
+    required: true,
+  },
+  description: String,
+  completed: Boolean,
+  created_at: { type: Date, default: Date.now },
+});
 
-const Project = mongoose.model('Project', ProjectSchema)
+const Project = mongoose.model("Project", ProjectSchema);
 
-const AdminBro = require('admin-bro')
-const AdminBroExpress = require('@admin-bro/express')
-const AdminBroMongoose = require('@admin-bro/mongoose')
+const AdminBro = require("admin-bro");
+const AdminBroExpress = require("@admin-bro/express");
+const AdminBroMongoose = require("@admin-bro/mongoose");
 
-AdminBro.registerAdapter(AdminBroMongoose)
+AdminBro.registerAdapter(AdminBroMongoose);
 
 const adminBroOptions = new AdminBro({
-    resources: [
-        {
-            resource: Project,
-            options: {
-                properties: {
-                    description: { type: 'richtext' },
-                    created_at: {
-                        isVisible: { edit: false, list: true, show: true, filter: true }
-                    }
-                }
-            }
-        }
-    ],
-    locale: {
-        translations: {
-            labels: {
-                Project: 'My projects'
-            }
-        }
+  resources: [
+    {
+      resource: Project,
+      options: {
+        properties: {
+          description: { type: "richtext" },
+          created_at: {
+            isVisible: { edit: false, list: true, show: true, filter: true },
+          },
+        },
+      },
     },
-    rootPath: '/admin'
-})
+  ],
+  locale: {
+    translations: {
+      labels: {
+        Project: "My projects",
+      },
+    },
+  },
+  rootPath: "/admin",
+});
 
-const router = AdminBroExpress.buildRouter(adminBroOptions)
+const router = AdminBroExpress.buildRouter(adminBroOptions);
 
-const express = require('express')
-const server = express()
+const express = require("express");
+const server = express();
 
-server.use(adminBroOptions.options.rootPath, router)
+server.use(adminBroOptions.options.rootPath, router);
 
 const run = async () => {
-    await mongoose.connect('mongodb://localhost/adminbroapp', {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    })
+  await mongoose.connect("mongodb://localhost/adminbroapp", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
 
-    await server.listen(5500, () => console.log('Server started'))
-}
+  await server.listen(5500, () => console.log("Server started"));
+};
 
-run()
+run();
